3532732a5777e13a582f465dc38fcd1e3eee9e77
StevenChang805 <changs@isyedu.org>
2019-10-02 21:58:19 +0630

    Cleaned up Code, Optimized updateStatus, and current magnitude as well as ocean size cannot be adjusted after LandMode is toggled.

--- ISY-SIM/isy/sim/FXMLSetUpController.java
+++ ISY-SIM/isy/sim/FXMLSetUpController.java
@@ -27,6 +27,7 @@ import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 import com.sun.deploy.panel.TextFieldProperty;
+import javafx.beans.InvalidationListener;
 import javafx.beans.binding.Binding;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
@@ -77,15 +78,7 @@ public class FXMLSetUpController implements Initializable {
     private double oceanHeight=500;
     private double horizontalSpeed = 2;
     private double verticalSpeed = 2;
-
-    private StringProperty txtHor;
-    private DoubleProperty sldHor;
-    private StringConverter<Number> convHorizontal;
-
-    private StringProperty txtVer;
-    private DoubleProperty sldVer;
-    private StringConverter<Number> convVertical;
-
+    private boolean landToggled = false;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -107,45 +100,33 @@ public class FXMLSetUpController implements Initializable {
     }
 
     private void initializeSliders() {
-        txtHor = txtHorizontal.textProperty();
-        sldHor = sldHorizontal.valueProperty();
-        convHorizontal = new NumberStringConverter();
-        txtVer = txtVertical.textProperty();
-        sldVer = sldVertical.valueProperty();
-        convVertical = new NumberStringConverter();
-
-        Bindings.bindBidirectional(txtHor, sldHor, convHorizontal);
-        Bindings.bindBidirectional(txtVer, sldVer, convVertical);
-
-        sldHorizontal.valueProperty().addListener(new ChangeListener<Number>() {
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (currentToggle) {
-                    setCurrentHorizontal(newValue.doubleValue());
-                    drawOcean();
-                    sldHorizontal.setValue(horizontalSpeed);
-
-                } else {
-                    setOceanWidth(newValue.doubleValue());
-                    updateStatus();
-                    oceanWidth = sldHorizontal.getValue();
-                }
-                txtHorizontal.setText(String.format("%d", newValue.intValue()));
+        sldHorizontal.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (currentToggle && !landToggled) {
+                setCurrentHorizontal(newValue.doubleValue());
+                drawOcean();
+                sldHorizontal.setValue(horizontalSpeed);
+                txtHorizontal.setText(""+horizontalSpeed);
+            } else {
+                setOceanWidth(newValue.doubleValue());
+                oceanWidth = sldHorizontal.getValue();
+                txtHorizontal.setText(String.format("%d", (int) oceanWidth));
+                updateStatus();
             }
         });
 
-        sldVertical.valueProperty().addListener(new ChangeListener<Number>() {
-            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-                if (currentToggle) {
-                    setCurrentVertical(newValue.doubleValue());
-                    drawOcean();
-                    sldVertical.setValue(verticalSpeed);
-                } else {
-                    setOceanHeight(newValue.doubleValue());
-                    updateStatus();
-                    oceanHeight = sldVertical.getValue();
-                }
-                txtVertical.setText(String.format("%d", (newValue.intValue())));
+        sldVertical.valueProperty().addListener((observable, oldValue, newValue) -> {
+            if (currentToggle && !landToggled) {
+                setCurrentVertical(newValue.doubleValue());
+                drawOcean();
+                sldVertical.setValue(verticalSpeed);
+                txtHorizontal.setText(""+horizontalSpeed);
+            } else {
+                setOceanHeight(newValue.doubleValue());
+                oceanHeight = sldVertical.getValue();
+                txtVertical.setText(String.format("%d", (int) oceanHeight));
+                updateStatus();
             }
+
         });
     }
     private void drawOcean(){
@@ -156,17 +137,21 @@ public class FXMLSetUpController implements Initializable {
         updateStatus();
     }
     private void setOceanWidth(double width) {
-        double scale = sldHorizontal.getWidth() / 900;
-        double oWidth = (width-100)*scale+60;
-        cnvOcean.setWidth(oWidth);
-        drawOcean();
+        if (!landToggled) {
+            double scale = sldHorizontal.getWidth() / 900;
+            double oWidth = (width-100)*scale+60;
+            cnvOcean.setWidth(oWidth);
+            drawOcean();
+        }
     }
     private void setOceanHeight(double height) {
-        double scale = sldVertical.getHeight() / 900;
-        double oHeight = (height-100)*scale+50;
-        cnvOcean.setHeight(oHeight);
-        cnvOcean.setTranslateY(525-oHeight);
-        drawOcean();
+        if (!landToggled) {
+            double scale = sldVertical.getHeight() / 900;
+            double oHeight = (height-100)*scale+50;
+            cnvOcean.setHeight(oHeight);
+            cnvOcean.setTranslateY(525-oHeight);
+            drawOcean();
+        }
     }
     private void drawGridLines(GraphicsContext gc, int minorGL, int majorGL) {
         gc.setStroke(Color.BLUE);
@@ -255,6 +240,8 @@ public class FXMLSetUpController implements Initializable {
                 setSldVerMinMax(1, verticalSpeed);
                 setSldVerMinMax(1, 10);
             } else {
+                setSldHorMinMax((int)oceanWidth, 1000);
+                setSldVerMinMax((int)oceanHeight, 1000);
                 setSldHorMinMax(100, 1000);
                 setSldVerMinMax(100, 1000);
                 txtHorizontal.setText(String.valueOf(oceanWidth));
@@ -265,8 +252,8 @@ public class FXMLSetUpController implements Initializable {
     private void toggleLand() {
         btnLand.selectedProperty().addListener(((observable, oldValue, newValue) -> {
             landToggle = !landToggle;
+            landToggled = true;
             if (landToggle) {
-                System.out.println("Point 1 Reached.");
                 cnvOcean.setOnMouseClicked(new EventHandler<MouseEvent>() {
                     @Override
                     public void handle(MouseEvent event) {
@@ -317,15 +304,16 @@ public class FXMLSetUpController implements Initializable {
         } else {
             action="Changing Size";
         }
-        statusBar.setText("Action: " + action + "\t Size: "+(int)oceanWidth+"x"+(int)oceanHeight+'\t' + "Current Speed:" + (int)horizontalSpeed+" x "+(int)verticalSpeed + "\nLand amount:" + "\tWaste amount:");
+        double[] gridCoords = convertMouseToGrid(cnvOcean.getWidth(), cnvOcean.getHeight());
+        statusBar.setText("Action: " + action + "\t Size: "+(int)gridCoords[0]+"x"+(int)gridCoords[1]+'\t' + "Current Speed:" + (int)horizontalSpeed+" x "+(int)verticalSpeed + "\nLand amount:" + "\tWaste amount:");
     }
 
     private void generateIsland(GraphicsContext gc, MouseEvent mouseEvent, int size) {
         double xCoordinate = mouseEvent.getX();
         double yCoordinate = mouseEvent.getY();
 
-        double[] xCoordinates = {xCoordinate, xCoordinate, xCoordinate+size, xCoordinate+size};
-        double[] yCoordinates = {yCoordinate, yCoordinate+size, yCoordinate+size, yCoordinate};
+        double[] xCoordinates = {xCoordinate-size/2.0, xCoordinate-size/2.0, xCoordinate+size/2.0, xCoordinate+size/2.0};
+        double[] yCoordinates = {yCoordinate-size/2.0, yCoordinate+size/2.0, yCoordinate+size/2.0, yCoordinate-size/2.0};
 
         gc.setFill(Color.GREEN);
         gc.fillPolygon(xCoordinates, yCoordinates, 4);
@@ -341,17 +329,13 @@ public class FXMLSetUpController implements Initializable {
 //        double[] yPoints = {(double) length*random.nextInt(heightCoef), (double) length*random.nextInt(heightCoef), (double) length*random.nextInt(heightCoef)};
 //    }
     private double[] convertMouseToGrid(double mouseX, double mouseY) {
-        double canvasX = mouseX - 60;
-        double canvasY = 525 - mouseY;
-        double gridX = canvasX * 1.35135;
-        double gridY = canvasY * 1.90476;
+        double gridX = mouseX*1.323529411764706+20.588235294117647;
+        double gridY = mouseY*1.894736842105263+5.263157894736842;
         return new double[]{gridX, gridY};
     }
     private double[] convertGridToMouse(double gridX, double gridY) {
-        double canvasX = gridX/1.35135;
-        double canvasY = gridY/1.90476;
-        double mouseX = canvasX + 60;
-        double mouseY = 525 - canvasY;
+        double mouseX = (gridX-20.588235294117647)/1.323529411764706;
+        double mouseY = (gridY-5.263157894736842)/1.894736842105263;
         return new double[]{mouseX, mouseY};
     }
 }
